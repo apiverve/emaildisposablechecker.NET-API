7f3e93376ce2b91a848d3f177beab7be6bac5037
APIVerve Team <hello@apiverve.com>
2024-09-21 01:10:11 -0500

    Bug Fixes

---
+++ DisposableEmailCheckerQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DisposableEmailCheckerQueryOptions {
+public string email { get; set; }
+}
+
+}
